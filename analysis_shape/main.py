359afb674c625c30154123e954e8efbe808cfc17
Merlin Baviere <merlin.baviere@student-cs.fr>
2025-03-26 17:16:27 +0100

    standby

--- analysis_shape/main.py
+++ analysis_shape/main.py
@@ -3,8 +3,9 @@ import os
 
 # Imports des autres fichiers
 from src.viewshed import viewsheds_create
-from src.area_analysis import coverage, coverage_out_of_total_coverage
+from src.area_analysis import coverage, coverage_out_of_total_coverage, reccurent_coverage
 from src.utils import display_tif, normalize, fusion_or, fusion_and, create_template, write_data
+
 from qgis.core import QgsProject
 
 # Setup
@@ -31,23 +32,27 @@ layer_root = QgsProject.instance().layerTreeRoot()
 
 OUTPUT_PATH = os.path.join(DATA_PATH, "output.csv")
 
-# Create viewpoints & viewsheds
-viewsheds_create(cvs_path = CSV_PATH, dem_path = DEM_PROJECTED_PATH, elevation_style_file = ELEVATION_MODEL_PATH, output = VIEWSHEDS_PATH, layer_tree_root = layer_root)
+# # Create viewpoints & viewsheds
+# viewsheds_create(cvs_path = CSV_PATH, dem_path = DEM_PROJECTED_PATH, elevation_style_file = ELEVATION_MODEL_PATH, output = VIEWSHEDS_PATH, layer_tree_root = layer_root)
 
 
-# Créer une version normalisée des viewsheds pour pouvoir faire le calcul total 
-for file_name in os.listdir(VIEWSHEDS_PATH):
-    if file_name.endswith(".tif"):
-        file_path = os.path.join(VIEWSHEDS_PATH, f"{file_name}").replace("\\", "/")
-        normalize(file_path, output_path = NORM_VIEWSHEDS_PATH)
+# # Create a normalized version of the viewsheds to compute area covered
+# for file_name in os.listdir(VIEWSHEDS_PATH):
+#     if file_name.endswith(".tif"):
+#         file_path = os.path.join(VIEWSHEDS_PATH, f"{file_name}").replace("\\", "/")
+#         normalize(file_path, output_path = NORM_VIEWSHEDS_PATH)
 
-# Create a fusion file to compute total area covered
+# # Create a fusion file to compute total area covered
 norm_tif_files = [os.path.join(NORM_VIEWSHEDS_PATH, f).replace("\\", "/") for f in os.listdir(NORM_VIEWSHEDS_PATH) if f.endswith(".tif")]
-fusion_or(norm_tif_files, os.path.join(FUSION_PATH, "fusion_or_all.tif"))
+# fusion_or(norm_tif_files, os.path.join(FUSION_PATH, "fusion_or_all.tif"))
 
 
 # Afficher la surface totale couverte par l'ensemble des viewsheds sur une couche QGIS
 display_tif(os.path.join(FUSION_PATH, "fusion_or_all.tif"))
+
+# Calculer la surface totale couverte
+surface = coverage(os.path.join(FUSION_PATH, "fusion_or_all.tif"))
+print(f"Surface totale couverte : {surface} m²\n")
 
 ## Dictionnaire avec les valeurs de sortie
 output = {} 
@@ -56,43 +61,16 @@ output = {}
 for path in norm_tif_files:
     name = os.path.basename(path).replace("norm_viewshed_", "").rsplit(".", 1)[0]
     
-    output[name] = {"Surface" : coverage(path)}
-    print(f"Surface couverte par {name} ajoutée au dictionnaire")
-
-## Dictionnaire avec les valeurs de sortie
-output = {} 
-
-# Initialisation du dictionnaire (Surtout à ne pas enlever)
-for path in norm_tif_files:
-    name = os.path.basename(path).replace("norm_viewshed_", "").rsplit(".", 1)[0]
-    output.setdefault(name, {})
-    output[name].setdefault("Surface", None)
-    output[name].setdefault("p_surface", None)
-    output[name].setdefault("r_surface", None)
-    for path2 in norm_tif_files:
-        name2 = os.path.basename(path2).replace("norm_viewshed_", "").rsplit(".", 1)[0]
-        output[name].setdefault(name2, None)
-
-for path in norm_tif_files:
-    name = os.path.basename(path).replace("norm_viewshed_", "").rsplit(".", 1)[0]
-    
+    output[name] = {}
     output[name]["Surface"] = coverage(path)
+
     output[name]["p_surface"] = coverage_out_of_total_coverage(path, os.path.join(FUSION_PATH, "fusion_or_all.tif"))
 
     other_paths = [x for x in norm_tif_files if x != path]
 
-    # Recouvrement 2 à 2
-    output[name].setdefault(name, 1)
-    for path2 in other_paths:
-        name2 = os.path.basename(path2).replace("norm_viewshed_", "").rsplit(".", 1)[0]
-        if output[name][name2] == None:
-            fusion_22_path = os.path.join(FUSION_PATH, f"fusion_and_{name}_{name2}.tif")
-            fusion_and([path, path2],fusion_22_path)
-            cov = coverage(fusion_22_path)
-            output[name][name2] = cov
-            output[name2][name] = cov  
-    print(f"{name} ajouté au dictionnaire")  
-
+    print(f"{name} ajouté au dictionnaire")
+    break
+    
 columns = ["Nom", "Latitude", "Longitude", "Hauteur"]
 create_template(CSV_PATH, OUTPUT_PATH, columns)
 write_data(OUTPUT_PATH, output)
