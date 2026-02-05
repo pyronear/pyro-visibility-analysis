bca2e81471e9c01b89b3e72c1a2f8eed2f517c31
fe51 <55736935+fe51@users.noreply.github.com>
2026-02-05 17:09:48 +0100

    small refacto

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from analysis_shape import utils
+from analysis_shape.area_analysis import covered_surface
+from analysis_shape.utils import display_tif, normalize_create, create_template, write_data, add_osm_background
+from analysis_shape.viewshed import viewsheds_create
+from qgis.core import (
+    QgsProject,
+    QgsCoordinateReferenceSystem
+)
+from analysis_shape.utils import display_tif
+from pathlib import Path
 import os
 import csv
 import importlib
@@ -10,66 +20,47 @@ import importlib
 # importlib.reload(analysis_shape.utils)
 # importlib.reload(analysis_shape.viewshed)
 
-# === Function Imports ===
-from analysis_shape.viewshed import viewsheds_create
-from analysis_shape.utils import display_tif, normalize_create, create_template, write_data
-from analysis_shape.area_analysis import covered_surface
-
-from qgis.core import (
-    QgsProject,
-    QgsRasterLayer,
-    QgsLayerTreeLayer,
-    QgsCoordinateReferenceSystem
-)
-
-# === Add OpenStreetMap background layer ===
-def add_osm_background():
-    osm_url = "type=xyz&url=https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-    layer = QgsRasterLayer(osm_url, "OpenStreetMap", "wms")
-
-    if not layer.isValid():
-        print("❌ Failed to load OpenStreetMap layer")
-        return
-
-    # Add to bottom of layer tree
-    QgsProject.instance().addMapLayer(layer, False)
-    layer_node = QgsLayerTreeLayer(layer)
-    root = QgsProject.instance().layerTreeRoot()
-    root.insertChildNode(0, layer_node)
-
-    print("🗺️ OpenStreetMap added as background")
 
 # === Paths Setup ===
-VISILITY_ANALYSIS_PATH = os.path.dirname(__file__)
+try:
+    BASE_PATH = Path(__file__).resolve().parent
+except NameError:
+    BASE_PATH = Path(QgsProject.instance().fileName()).parent
+
+# VISILITY_ANALYSIS_PATH = os.path.dirname(__file__)
+VISILITY_ANALYSIS_PATH = BASE_PATH
+
 CSV_PATH = os.path.join(VISILITY_ANALYSIS_PATH, "data/sdis-67/sites.csv")
 OUTPUT_DIR = os.path.join(os.path.dirname(CSV_PATH), "output")
 
 VIEWSHEDS_PATH = os.path.join(OUTPUT_DIR, "viewsheds_geotiff")
 NORM_VIEWSHEDS_PATH = os.path.join(OUTPUT_DIR, "normalized")
+OBSERVATION_POINTS_PATH = os.path.join(OUTPUT_DIR, "observation_points")
 FUSION_PATH = os.path.join(OUTPUT_DIR, "fusion")
-ELEVATION_MODEL_PATH = os.path.join(OUTPUT_DIR, "Elevation_Model.qml")
+VIEWSHED_STYLE_FILE_PATH = os.path.join(OUTPUT_DIR, "viewshed_style.qml")
 DEM_PROJECTED_PATH = os.path.join(os.path.dirname(CSV_PATH), "dem_l93.tif")
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "output.csv")
 
 # Create output folders
 os.makedirs(VIEWSHEDS_PATH, exist_ok=True)
 os.makedirs(NORM_VIEWSHEDS_PATH, exist_ok=True)
+os.makedirs(OBSERVATION_POINTS_PATH, exist_ok=True)
 os.makedirs(FUSION_PATH, exist_ok=True)
 
 # === Set up QGIS Project ===
 project_crs = QgsCoordinateReferenceSystem("EPSG:2154")
 QgsProject.instance().setCrs(project_crs)
 
-# === Add OSM background first ===
+# === Add OpenStreetMap background layer ===
 add_osm_background()
 
 # === Run visibility analysis pipeline ===
 
 # 1. Create viewpoints & viewsheds
 viewsheds_create(
-    cvs_path=CSV_PATH,
+    csv_path=CSV_PATH,
     dem_path=DEM_PROJECTED_PATH,
-    elevation_style_file=ELEVATION_MODEL_PATH,
+    elevation_style_file=VIEWSHED_STYLE_FILE_PATH,
     output=VIEWSHEDS_PATH,
     layer_tree_root=QgsProject.instance().layerTreeRoot()
 )
@@ -91,4 +82,5 @@ print("✅ Analysis completed.")
 
 # 6. Display final fused viewshed
 CSV_name = os.path.splitext(os.path.basename(CSV_PATH))[0]
-display_tif(os.path.join(FUSION_PATH, f"fusion_or_all_{CSV_name}.tif"))
+display_tif(os.path.join(
+    FUSION_PATH, f"fusion_or_all_{CSV_name}.tif"), group_name="Viewsheds_merged", style_file_path=VIEWSHED_STYLE_FILE_PATH)
